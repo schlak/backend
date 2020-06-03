36dbe5a351c9fc6fff63785928f40ba5760d9f26
Harry Merritt <harry@merritt.es>
2020-06-04 00:25:17 +0100

    FIles helper functions
    Useful fs abstractions
    * exists
    * isFile
    * isDirectory
    * isTypeAudio

---
+++ indexer/Files.js
@@ -0,0 +1,50 @@
+"use strict";
+
+const fs = require("fs");
+
+/**
+ * File helper functions;
+ */
+class Files {
+    constructor() {}
+
+    /**
+     * Check if a file/directory exists (synchronously)
+     */
+    exists(path) {
+        if (fs.existsSync(path)) return true;
+        return false;
+    }
+
+    /**
+     * Check if item is a file
+     */
+    isFile(path) {
+        return this.exists(path) && fs.lstatSync(path).isFile();
+    }
+
+    /**
+     * Check if item is a directory
+     */
+    isDirectory(path) {
+        return this.exists(path) && fs.lstatSync(path).isDirectory();
+    }
+
+    /**
+     * Check if file is a music file
+     */
+    isTypeAudio(path) {
+        // Filter for music file extensions
+        if (
+            path.substr(path.length - 5) === ".flac" ||
+            path.substr(path.length - 4) === ".mp3" ||
+            path.substr(path.length - 4) === ".ogg"
+        ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+module.exports = new Files();
