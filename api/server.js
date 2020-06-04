995928a335fd87c018ec4d25d6a7953e5f945de4
Harry Merritt <harry@merritt.es>
2020-06-04 20:05:39 +0100

    add version number to base endpoint

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const routes = require("./routes");
+const pkg = require("../package.json");
 
 const server = express();
 
@@ -12,7 +13,8 @@ server.use("/", routes);
 // Base root welcome message
 server.get("/", (req, res) => {
     res.send({
-        message: "Hello World!",
+        message: "Hello, world!",
+        version: pkg.version,
         routes: [
             "/tracks",
             "/tracks/:id",
